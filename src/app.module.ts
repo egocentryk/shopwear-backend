327180608efd769e29585b3400bb08d7b0a32659
egocentryk <lukasz.skowron@gmail.com>
2025-02-19 20:55:20 +0100

    feat(): basic user files created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { DevtoolsModule } from '@nestjs/devtools-integration'
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     DevtoolsModule.register({
       http: process.env.NODE_ENV !== 'production',
     }),
+    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

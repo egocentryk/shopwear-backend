0c37edab41ccec7d96ecbb92ee616271e4cee920
egocentryk <lukasz.skowron@gmail.com>
2025-03-07 12:11:12 +0100

    feat(): serialized type convertion with class-transformer

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { Type } from 'class-transformer'
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
 
 export class CreateProductDto {
@@ -10,5 +11,6 @@ export class CreateProductDto {
   description: string
 
   @IsNumber()
+  @Type(() => Number)
   price: number
 }

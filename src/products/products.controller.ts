f6525c85a5a53319796acec525bfe095bc627a22
egocentryk <lukasz.skowron@gmail.com>
2025-03-31 11:59:26 +0200

    feat(products): getProducts method implemented, docker-compose file added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common'
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
 import { CreateProductDto } from './dto/create-product.dto'
 import { CurrentUser } from '../auth/current-user.decorator'
@@ -16,5 +16,11 @@ export class ProductsController {
     @CurrentUser() user: TokenPayload,
   ) {
     return this.productsService.createProduct(createProductDto, user.userId)
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getProducts() {
+    return this.productsService.getProducts()
   }
 }
